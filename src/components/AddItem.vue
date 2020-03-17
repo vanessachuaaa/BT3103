13c3572d6591e967bb0a494de0d6e7249159566b
vanessachuaaa <vanessachuaaa.rt@gmail.com>
2020-03-17 08:36:18 +0800

    adding Files

--- src/components/AddItem.vue
+++ src/components/AddItem.vue
@@ -2,12 +2,22 @@
   <div id=app>
     <h1>Add Item Page</h1>
     <form id="fm1">
-        <label>Item Name</label>
+        <label>Item Name:</label>
         <input type="text" v-model.lazy="item.name" required/>
-        <label>Item Category</label>
-        <input type="text" v-model.lazy="item.category"/>
-        <button v-on:click.prevent="addItem">Add Item</button>
+        <br>
+
+        <label> Item Category: </label>
+        <input type="checkbox" id ="checkbox1" v-model.lazy="item.category" value = "Snack"/><label for="checkbox1">Snack</label>
+        <input type="checkbox" id ="checkbox2" v-model.lazy="item.category" value = "Meal"/> <label for="checkbox2">Meal</label>
+        <input type="checkbox" id ="checkbox3" v-model.lazy="item.category" value = "Tea Break"/><label for="checkbox3">Tea Break</label>
+        <br>
+     
+        <label> Item Type: </label>
+        <input type="radio" id="optionA" name="radioBtn" value="Food" v-model.lazy="item.type" /><label for="optionA">Food</label>
+        <input type="radio" id="optionB" name="radioBtn" value="Drink" v-model.lazy="item.type" /><label for="optionB">Drink</label>
         
+        <br>
+        <button v-on:click.prevent="addItem">Add Item</button>
     </form>
   </div>
 </template>
@@ -22,18 +32,20 @@ export default {
         msg:"Add Item",
         item:{
           name:'',
-          category:''
+          category: [],
+          type: '',
         },
         
         
-        }
+      }
   },
   methods:{
     addItem:  function () {
           //Save item to database
           database.collection('items').doc().set(this.item);
           this.item.name="";
-          this.item.category="";
+          this.item.category= [];
+          this.item.type = "";
           alert("I am in the DB .... :-) Item saved successfully")
           
         }
@@ -48,7 +60,7 @@ export default {
 }
 #app{
     margin: 20px auto;
-    max-width: 500px;
+    max-width: 700px;
 }
 
 p{
@@ -58,7 +70,7 @@ p{
 label{
     display: inline-block;
     margin: 20px 0 10px;
-    width:50%;
+    width:15%;
     align-content:left;
 
 }
@@ -66,5 +78,8 @@ input[type="text"]{
     display: inline-block;
     padding: 8px;
     width:50%;
+}
+input[type = "checkbox"] {
+  width:5%;
 }
 </style>
